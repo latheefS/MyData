3492ca6c4d52e1252837ae4aadba660a6384a93b
Latheef <latheef.s93@gmail.com>
2024-11-19 06:47:30 +0530

    MPD1

---
+++ My Files/Python Files/pythonProjectdemo/Demo.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Input the CSV file path and read the CSV file into a pandas DataFrame
+csv_file_path = r"C:\Users\LatheefS\Desktop\PythonAI\input\x_ve1_maximise_test_results.csv"
+df = pd.read_csv(csv_file_path)
